795a16937e41b2c5145c24fb72332bafaff23e27
Harshvardhan Shete <harshvardhan@logibricks.in>
2022-05-20 10:33:03 +0530

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { productsData, productsDataTwo } from "./components/Products/data";
 import Feature from "./components/Feature";
 import Footer from "./components/Footer";
 
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   // const logIn = () => {
@@ -22,32 +22,26 @@ function App() {
   // };
 
   return (
+    <Routes>
+      <Route
+        path="/cafe-peter/landingPage"
+        exact
+        element={
+          <Protected isLoggedIn={isLoggedIn}>
+            {/* <CafePeterLandingPage /> */}
 
-    <Router>
-      <Routes>
-        <Route
-          path="/cafe-peter/landingPage"
-          exact
-          element={
-            <Protected isLoggedIn={isLoggedIn}>
-              {/* <CafePeterLandingPage /> */}
-              
-                <GlobalStyle />
-                <Hero />
-                <Products heading="Choose your favorite" data={productsData} />
-                <Feature />
-                <Products
-                  heading="Sweet Treats for You"
-                  data={productsDataTwo}
-                />
-                <Footer />
-            </Protected>
-          }
-        />
-        {/* <Route path="/" element={<InfoForm />} /> */}
-        <Route path="/cafe-peter" exact element={<InfoForm />} />
-      </Routes>
-    </Router>
+            <GlobalStyle />
+            <Hero />
+            <Products heading="Choose your favorite" data={productsData} />
+            <Feature />
+            <Products heading="Sweet Treats for You" data={productsDataTwo} />
+            <Footer />
+          </Protected>
+        }
+      />
+      {/* <Route path="/" element={<InfoForm />} /> */}
+      <Route path="/cafe-peter" exact element={<InfoForm />} />
+    </Routes>
   );
 }
 export default App;
